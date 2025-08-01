355bdc537b7825d7028281e305ef81cefca1146b
Andrew Blair <andrew.blair@xplortechnologies.com>
2025-08-01 19:13:31 -0400

    fix: resolve errors blocking build

--- globals.js
+++ globals.js
@@ -1,37 +1,37 @@
 /* @flow */
 /* eslint import/no-commonjs: off */
 
+const postRobotGlobals = require("post-robot/globals");
 
-const postRobotGlobals = require('post-robot/globals');
-
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 const formatVersion = (version) => {
-    return version.replace(/[^\d]+/g, '_');
+  return version.replace(/[^\d]+/g, "_");
 };
 
-//logs to track Zoid component lifecycle - initialization and version formatting
-//eslint-disable-next-line no-console
-console.warn('ZOID Component Initialized with version:', pkg.version);
-//eslint-disable-next-line no-console
-console.error('Formatted ZOID Version:', formatVersion(pkg.version));
+// logs to track Zoid component lifecycle - initialization and version formatting
+// eslint-disable-next-line no-console
+console.warn("ZOID Component Initialized with version:", pkg.version);
+// eslint-disable-next-line no-console
+console.error("Formatted ZOID Version:", formatVersion(pkg.version));
 
 module.exports = {
-    __POST_ROBOT__: {
-        ...postRobotGlobals.__POST_ROBOT__,
-        __AUTO_SETUP__: false
+  __POST_ROBOT__: {
+    ...postRobotGlobals.__POST_ROBOT__,
+    __AUTO_SETUP__: false,
+  },
+  __ZOID__: {
+    __VERSION__: `${formatVersion(pkg.version)}`,
+    __GLOBAL_KEY__: `__zoid_${formatVersion(pkg.version)}__`,
+    __IFRAME_SUPPORT__: true,
+    __POPUP_SUPPORT__: true,
+    __FRAMEWORK_SUPPORT__: false,
+    __DEFAULT_CONTAINER__: true,
+    __DEFAULT_PRERENDER__: true,
+    __SCRIPT_NAMESPACE__: false,
+    onError: (err) => {
+      /* eslint-disable-next-line no-console */
+      console.error(`[ZOID] Global error: ${err.message}`);
     },
-    __ZOID__: {
-        __VERSION__:           `${ formatVersion(pkg.version) }`,
-        __GLOBAL_KEY__:        `__zoid_${ formatVersion(pkg.version) }__`,
-        __IFRAME_SUPPORT__:    true,
-        __POPUP_SUPPORT__:     true,
-        __FRAMEWORK_SUPPORT__: false,
-        __DEFAULT_CONTAINER__: true,
-        __DEFAULT_PRERENDER__: true,
-        __SCRIPT_NAMESPACE__:  false,
-      onError: (err) => {
-            console.error(`[ZOID] Global error: ${err.message}`);
-        }
-    }
+  },
 };
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 /* @flow */
 /* eslint import/no-commonjs: 0 */
 
-//eslint-disable-next-line no-console
-console.log('Loading Zoid module:', process.env.ZOID_FRAME_ONLY ? 'zoid.frame' : 'zoid');
+// eslint-disable-next-line no-console
+console.log(
+  "Loading Zoid module:",
+  // eslint-disable-next-line no-process-env
+  process.env.ZOID_FRAME_ONLY ? "zoid.frame" : "zoid"
+);
 
 // eslint-disable-next-line no-process-env
 if (process && process.env && process.env.ZOID_FRAME_ONLY) {
-    // $FlowFixMe
-    module.exports = require('./dist/zoid.frame');
-    // $FlowFixMe
-    module.exports.default = module.exports;
+  // $FlowFixMe
+  module.exports = require("./dist/zoid.frame");
+  // $FlowFixMe
+  module.exports.default = module.exports;
 } else {
-    // $FlowFixMe
-    module.exports = require('./dist/zoid');
-    // $FlowFixMe
-    module.exports.default = module.exports;
+  // $FlowFixMe
+  module.exports = require("./dist/zoid");
+  // $FlowFixMe
+  module.exports.default = module.exports;
 }
