febbca5d48a820a0a4093e1c60b26e16367ffb43
Andrew Blair <andrew.blair@xplortechnologies.com>
2025-08-01 16:33:13 -0400

    Update files with debug logging for Mariana Django

--- globals.js
+++ globals.js
@@ -1,27 +1,34 @@
 /* @flow */
 /* eslint import/no-commonjs: off */
 
-const postRobotGlobals = require("@krakenjs/post-robot/globals");
 
-const pkg = require("./package.json");
+const postRobotGlobals = require('post-robot/globals');
+
+const pkg = require('./package.json');
 
 const formatVersion = (version) => {
-  return version.replace(/[^\d]+/g, "_");
+    return version.replace(/[^\d]+/g, '_');
 };
 
+//logs to track Zoid component lifecycle - initialization and version formatting
+//eslint-disable-next-line no-console
+console.warn('ZOID Component Initialized with version:', pkg.version);
+//eslint-disable-next-line no-console
+console.error('Formatted ZOID Version:', formatVersion(pkg.version));
+
 module.exports = {
-  __POST_ROBOT__: {
-    ...postRobotGlobals.__POST_ROBOT__,
-    __AUTO_SETUP__: false,
-  },
-  __ZOID__: {
-    __VERSION__: `${formatVersion(pkg.version)}`,
-    __GLOBAL_KEY__: `__zoid_${formatVersion(pkg.version)}__`,
-    __IFRAME_SUPPORT__: true,
-    __POPUP_SUPPORT__: true,
-    __FRAMEWORK_SUPPORT__: false,
-    __DEFAULT_CONTAINER__: true,
-    __DEFAULT_PRERENDER__: true,
-    __SCRIPT_NAMESPACE__: false,
-  },
+    __POST_ROBOT__: {
+        ...postRobotGlobals.__POST_ROBOT__,
+        __AUTO_SETUP__: false
+    },
+    __ZOID__: {
+        __VERSION__:           `${ formatVersion(pkg.version) }`,
+        __GLOBAL_KEY__:        `__zoid_${ formatVersion(pkg.version) }__`,
+        __IFRAME_SUPPORT__:    true,
+        __POPUP_SUPPORT__:     true,
+        __FRAMEWORK_SUPPORT__: false,
+        __DEFAULT_CONTAINER__: true,
+        __DEFAULT_PRERENDER__: true,
+        __SCRIPT_NAMESPACE__:  false
+    }
 };
--- src/child/props.js
+++ src/child/props.js
@@ -1,128 +1,64 @@
 /* @flow */
 
-import {
-  getDomain,
-  isSameDomain,
-  matchDomain,
-  type CrossDomainWindowType,
-} from "@krakenjs/cross-domain-utils/src";
+import { getDomain, isSameDomain, type CrossDomainWindowType } from 'cross-domain-utils/src';
 
-import type {
-  PropsDefinitionType,
-  PropsType,
-  ChildPropsType,
-} from "../component/props";
+import type { PropsDefinitionType, PropsType, ChildPropsType } from '../component/props';
 
-import type { ChildHelpers } from "./index";
+import type { ChildHelpers } from './index';
 
-export function normalizeChildProp<P, T, X>(
-  // $FlowFixMe
-  propsDef: PropsDefinitionType<P, X>,
-  props: PropsType<P>,
-  key: string,
-  value: ?T,
-  helpers: ChildHelpers<P, X>
-): ?T {
-  if (!propsDef.hasOwnProperty(key)) {
+// $FlowFixMe
+export function normalizeChildProp<P, T, X>(propsDef : PropsDefinitionType<P, X>, props : PropsType<P>, key : string, value : ?T, helpers : ChildHelpers<P, X>) : ?T  {
+    //es-lint-disable-next-line no-console
+    console.info(`Normalizing child prop: ${key}`, { value });
+    if (!propsDef.hasOwnProperty(key)) {
+        return value;
+    }
+
+    const prop = propsDef[key];
+
+    if (typeof prop.childDecorate === 'function') {
+        const { uid, tag, close, focus, onError, onProps, resize, getParent, getParentDomain, show, hide, export: xport, getSiblings } = helpers;
+        const decoratedValue = prop.childDecorate({ value, uid, tag, close, focus, onError, onProps, resize, getParent, getParentDomain, show, hide, export: xport, getSiblings });
+
+        // $FlowFixMe
+        return decoratedValue;
+    }
+
     return value;
-  }
-
-  const prop = propsDef[key];
-
-  if (typeof prop.childDecorate === "function") {
-    const {
-      uid,
-      tag,
-      close,
-      focus,
-      onError,
-      onProps,
-      resize,
-      getParent,
-      getParentDomain,
-      show,
-      hide,
-      export: xport,
-      getSiblings,
-    } = helpers;
-    const decoratedValue = prop.childDecorate({
-      value,
-      uid,
-      tag,
-      close,
-      focus,
-      onError,
-      onProps,
-      resize,
-      getParent,
-      getParentDomain,
-      show,
-      hide,
-      export: xport,
-      getSiblings,
-    });
-
-    // $FlowFixMe
-    return decoratedValue;
-  }
-
-  return value;
 }
 
 // eslint-disable-next-line max-params
-export function normalizeChildProps<P, X>(
-  parentComponentWindow: CrossDomainWindowType,
-  propsDef: PropsDefinitionType<P, X>,
-  props: PropsType<P>,
-  origin: string,
-  helpers: ChildHelpers<P, X>,
-  isUpdate: boolean = false
-): ChildPropsType<P, X> {
-  const result = {};
+export function normalizeChildProps<P, X>(parentComponentWindow : CrossDomainWindowType, propsDef : PropsDefinitionType<P, X>, props : PropsType<P>, origin : string, helpers : ChildHelpers<P, X>, isUpdate : boolean = false) : ChildPropsType<P, X> {
+    //es-lint-disable-next-line no-console
+    console.error('Normalizing child props:', { props });
 
-  for (const key of Object.keys(props)) {
-    const prop = propsDef[key];
+    const result = {};
 
-    const trustedChild: boolean =
-      prop && prop.trustedDomains && prop.trustedDomains.length > 0
-        ? prop.trustedDomains.reduce((acc, val) => {
-            return acc || matchDomain(val, getDomain(window));
-          }, false)
-        : origin === getDomain(window) || isSameDomain(parentComponentWindow);
+    for (const key of Object.keys(props)) {
+        const prop = propsDef[key];
 
-    // let trustedDomains override sameDomain prop
-    if (prop && prop.sameDomain && !trustedChild) {
-      continue;
+        if (prop && prop.sameDomain && (origin !== getDomain(window) || !isSameDomain(parentComponentWindow))) {
+            continue;
+        }
+
+        // $FlowFixMe
+        const value = normalizeChildProp(propsDef, props, key, props[key], helpers);
+
+        result[key] = value;
+        if (prop && prop.alias && !result[prop.alias]) {
+            result[prop.alias] = value;
+        }
     }
 
-    // sameDomain was not set and trusted domains must match
-    if (prop && prop.trustedDomains && !trustedChild) {
-      continue;
+    if (!isUpdate) {
+        console.log('Initial props normalization complete:', { result });
+        for (const key of Object.keys(propsDef)) {
+            if (!props.hasOwnProperty(key)) {
+                result[key] = normalizeChildProp(propsDef, props, key, undefined, helpers);
+            }
+        }
     }
 
     // $FlowFixMe
-    const value = normalizeChildProp(propsDef, props, key, props[key], helpers);
-
-    result[key] = value;
-    if (prop && prop.alias && !result[prop.alias]) {
-      result[prop.alias] = value;
-    }
-  }
-
-  if (!isUpdate) {
-    for (const key of Object.keys(propsDef)) {
-      if (!props.hasOwnProperty(key)) {
-        result[key] = normalizeChildProp(
-          propsDef,
-          props,
-          key,
-          undefined,
-          helpers
-        );
-      }
-    }
-  }
-
-  // $FlowFixMe
-  return result;
+    return result;
 }
